11f9ff49493c3f6c7aa6837c5b3b583d7daac852
dlndn <65935030+dlndn@users.noreply.github.com>
2023-11-18 18:33:59 -0800

    added full game function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,10 +2,15 @@ let computerChoice = "";
 const ROCK = "rock";
 const PAPER = "paper";
 const SCISSORS = "scissors";
+const WIN = "win";
+const LOSE = "lose";
+const TIE = "tie";
+const TOTAL_ROUNDS = 5;
+const TOTAL_CHOICES = 3;
 
 function getComputerChoice() {
     let choices = [ROCK, PAPER, SCISSORS];
-    return choices[Math.floor(Math.random() * 3)];
+    return choices[Math.floor(Math.random() * TOTAL_CHOICES)];
 }
 
 function getPlayerChoice() {
@@ -33,18 +38,54 @@ function playRound(playerSelection, computerSelection) {
     if ((playerSelection == ROCK && computerSelection == PAPER) || 
         (playerSelection == PAPER && computerSelection == SCISSORS) ||
         (playerSelection == SCISSORS && computerSelection == ROCK)) {
-        result = "You lose! " + computerSelection + " beats " + playerSelection;
+        console.log("You lose! " + computerSelection + " beats " + playerSelection);
+        result = LOSE;
 
     //winning conditions
     } else if  ((playerSelection == ROCK && computerSelection == SCISSORS) || 
                 (playerSelection == PAPER && computerSelection == ROCK) ||
                 (playerSelection == SCISSORS && computerSelection == PAPER)) {
-        result = "You win! " + playerSelection + " beats " + computerSelection;
+        console.log("You win! " + playerSelection + " beats " + computerSelection);
+        result = WIN;
     
     //tie
     } else {
-        result = "It's a tie! You both picked " + playerSelection + ".";
+        console.log("It's a tie! You both picked " + playerSelection + ".");
+        result = TIE;
     }
 
     return result;
 }
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let roundResult = "";
+
+    for (let i=0; i<TOTAL_ROUNDS; i++) {
+        roundResult = playRound(getPlayerChoice(), getComputerChoice());
+
+        if (roundResult == WIN) {
+            playerScore++;
+
+        } else if (roundResult == LOSE) {
+            computerScore++;
+
+        } else {
+            playerScore++;
+            computerScore++;
+        }
+    }
+
+    if (playerScore > computerScore) {
+        console.log("Game over. You have won the game!");
+
+    } else if (playerScore < computerScore) {
+        console.log("Game over. You have lost the game!");
+
+    } else {
+        console.log("Game over. The game is tied!")
+    }
+}
+
+game();
