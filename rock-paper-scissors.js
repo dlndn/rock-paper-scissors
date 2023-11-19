2d0268e81fe940f71250394f997f225cf0bffc31
dlndn <65935030+dlndn@users.noreply.github.com>
2023-11-18 17:58:29 -0800

    added one round play

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,32 @@
 let computerChoice = "";
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
 
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = [ROCK, PAPER, SCISSORS];
     return choices[Math.floor(Math.random() * 3)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    let result = "";
+
+    //losing conditions
+    if ((playerSelection == ROCK && computerSelection == PAPER) || 
+        (playerSelection == PAPER && computerSelection == SCISSORS) ||
+        (playerSelection == SCISSORS && computerSelection == ROCK)) {
+        result = "You lose! " + computerSelection + " beats " + playerSelection;
+
+    //winning conditions
+    } else if  ((playerSelection == ROCK && computerSelection == SCISSORS) || 
+                (playerSelection == PAPER && computerSelection == ROCK) ||
+                (playerSelection == SCISSORS && computerSelection == PAPER)) {
+        result = "You win! " + playerSelection + " beats " + computerSelection;
+    
+    //tie
+    } else {
+        result = "It's a tie! You both picked " + playerSelection + ".";
+    }
+
+    return result;
 }
