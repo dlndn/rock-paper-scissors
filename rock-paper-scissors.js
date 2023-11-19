8054d805f63a8787925e6eaae6992d3526d539ed
dlndn <65935030+dlndn@users.noreply.github.com>
2023-11-18 18:16:49 -0800

    added player input

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,6 +8,24 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
+function getPlayerChoice() {
+    let playerChoice = "";
+
+    while (playerChoice == "") {
+        playerChoice = prompt("Please enter \"Rock\", \"Paper\", or \"Scissors\".").toLowerCase();
+        
+        if (playerChoice == ROCK ||
+            playerChoice == PAPER ||
+            playerChoice == SCISSORS) {
+            return playerChoice;
+
+        } else {
+            alert("Invalid input. Please only choose \"Rock\", \"Paper\", or \"Scissors\".");
+            playerChoice = ""; //clear input
+        }
+    }
+}
+
 function playRound(playerSelection, computerSelection) {
     let result = "";
 
